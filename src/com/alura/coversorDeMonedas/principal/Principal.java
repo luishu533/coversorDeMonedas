37eba80c7769a4003c25f2fbd054eca03519dfaf
Luis Huanca <huanca.luis@gmail.com>
2025-05-08 11:57:44 -0500

    se agrego bienvenida.

---
+++ src/com/alura/coversorDeMonedas/principal/Principal.java
@@ -0,0 +1,8 @@
+package com.alura.coversorDeMonedas.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al conversor de monedas");
+        System.out.println("Este sera la primera versión");
+    }
+}
