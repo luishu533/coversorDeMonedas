28b681821a1cd1347bf2783d4634bd042a01e236
Luis Huanca <huanca.luis@gmail.com>
2025-05-12 12:19:10 -0500

    se optimizó la estructura del main

--- src/com/alura/coversorDeMonedas/principal/Principal.java
+++ src/com/alura/coversorDeMonedas/principal/Principal.java
@@ -9,50 +9,49 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-
         ConsultaApi consultaApi = new ConsultaApi();
-        Scanner scanner = new Scanner(System.in);
-        Bienvenida.mostrarMensaje();
 
-        while (true) {
-            try {
-            System.out.println("\nIngresa la tasa de cambio que deseas consultar:");
-            String frase = scanner.nextLine();
-            if (frase.toLowerCase().trim().equals("salir")) {
-                System.out.println("Programa finalizado");
-                break;
-            }
+        try (Scanner scanner = new Scanner(System.in)) {
+            Bienvenida.mostrarMensaje();
+            while (true) {
+                try {
+                    System.out.println("\nIngresa la tasa de cambio que deseas consultar:");
+                    String frase = scanner.nextLine();
+                    if (frase.toLowerCase().trim().equals("salir")) {
+                        System.out.println("Programa finalizado");
+                        break;
+                    }
 
-            FraseConversor.Resultado resultado = FraseConversor.analizar(frase);
-            DecimalFormat formato = new DecimalFormat("#.##");
+                    FraseConversor.Resultado resultado = FraseConversor.analizar(frase);
+                    DecimalFormat formato = new DecimalFormat("#.##");
 
-            assert resultado != null;
+                    assert resultado != null;
 
-            Conversor moneda = consultaApi.consultar(
-                    DatosMoneda.obtenerCodigoIso(resultado.monedaOrigen()),
-                    DatosMoneda.obtenerCodigoIso(resultado.monedaDestino()),
-                    resultado.cantidad());
+                    Conversor moneda = consultaApi.consultar(
+                            DatosMoneda.obtenerCodigoIso(resultado.monedaOrigen()),
+                            DatosMoneda.obtenerCodigoIso(resultado.monedaDestino()),
+                            resultado.cantidad());
 
-            String fechaIngles = moneda.time_last_update_utc();
-            DateTimeFormatter formatoIngles = DateTimeFormatter.ofPattern(
-                    "EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-            ZonedDateTime fecha = ZonedDateTime.parse(fechaIngles, formatoIngles);
-            Locale localeEspanol = Locale.forLanguageTag("es-ES");
-            DateTimeFormatter formatoEspanol = DateTimeFormatter.ofPattern(
-                    "EEEE, dd MMMM yyyy",
-                    localeEspanol);
+                    String fechaIngles = moneda.time_last_update_utc();
+                    DateTimeFormatter formatoIngles = DateTimeFormatter.ofPattern(
+                            "EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+                    ZonedDateTime fecha = ZonedDateTime.parse(fechaIngles, formatoIngles);
+                    Locale localeEspanol = Locale.forLanguageTag("es-ES");
+                    DateTimeFormatter formatoEspanol = DateTimeFormatter.ofPattern(
+                            "EEEE, dd MMMM yyyy",
+                            localeEspanol);
 
-            System.out.println(
-                    resultado.cantidad() + " " +
-                            resultado.monedaOrigen() + " son: " +
-                            formato.format(moneda.conversion_result()) + " " +
-                            resultado.monedaDestino() + "\nÚltima actualización: " +
-                            fecha.format(formatoEspanol));
+                    System.out.println(
+                            resultado.cantidad() + " " +
+                                    resultado.monedaOrigen() + " son: " +
+                                    formato.format(moneda.conversion_result()) + " " +
+                                    resultado.monedaDestino() + "\nÚltima actualización: " +
+                                    fecha.format(formatoEspanol));
 
-            } catch (Exception e) {
-            System.out.println("cantidad no valida");
+                } catch (Exception e) {
+                    System.out.println("cantidad no valida");
+                }
             }
         }
-        scanner.close();
     }
 }
